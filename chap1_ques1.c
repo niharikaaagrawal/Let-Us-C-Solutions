f4a006d7508b66cb1dfff5973b740ab0151fa267
niharikaaagrawal <59929959+niharikaaagrawal@users.noreply.github.com>
2021-10-11 00:20:09 +0530

    Add files via upload

---
+++ chap1_ques1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	float F, C;
+	
+	printf("Enter the tempertaure : ");
+	scanf("%f", &F);
+	
+	C =  ((5/9) * (F - 32));
+	
+	printf("The temperature in celsius is: %f", &C);
+	
+	return 0;
+}
